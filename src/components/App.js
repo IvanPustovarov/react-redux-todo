6435c99261d7160d76a0f52c5f6805bc229417d2
WIllsnSmith <silverflash150@gmail.com>
2021-04-21 12:38:40 +0300

    based redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
+import React from 'react';
 
+import Header from './Header';
+import TodoList from './TodoList';
+import Footer from './Footer';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-     Анеки и шутки
+     <nav>
+        <section>
+          <h1>Redux Fundamentals Example</h1>
+        </section>
+      </nav>
+      <main>
+        <section className="medium-container">
+          <h2>Todos</h2>
+          <div className="todoapp">
+            <Header />
+            <TodoList />
+            <Footer />
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
